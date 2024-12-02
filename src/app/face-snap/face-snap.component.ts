67e5b6f93a5b4428d49d243eb381f3f7368f2ab6
CyrilJcb <jacobcyril042@gmail.com>
2024-12-02 15:58:59 +0100

    push attribute binding

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
@@ -7,6 +7,18 @@ import { Component } from '@angular/core';
   templateUrl: './face-snap.component.html',
   styleUrl: './face-snap.component.scss'
 })
-export class FaceSnapComponent {
+export class FaceSnapComponent implements OnInit{
+  title!: string;
+  description!: string;
+  createdAt!: Date;
+  snaps!: number;
+  imageURL!: string
 
+  ngOnInit(): void {
+    this.title = "Khyra et Upsy";
+    this.description = "photo d'elles";
+    this.createdAt = new Date();
+    this.snaps = 5;
+    this.imageURL = "https://www.empruntemontoutou.com/wp-content/uploads/2020/08/BRAQUE-WEIMAR.jpg";
+  }
 }
