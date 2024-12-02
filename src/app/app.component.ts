85c1fa5a5cd5a6d182bdb8c3edc9d9408175cb4c
CyrilJcb <jacobcyril042@gmail.com>
2024-12-02 11:06:08 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {FaceSnapComponent } from './face-snap/face-snap.component'
+
+
+@Component({
+  selector: 'app-root',
+  standalone:true,
+  imports: [FaceSnapComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  
+}
---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-face-snap',
+  standalone: true,
+  imports: [FaceSnapComponent],
+  templateUrl: './face-snap.component.html',
+  styleUrl: './face-snap.component.scss'
+})
+export class FaceSnapComponent {
+
+}
