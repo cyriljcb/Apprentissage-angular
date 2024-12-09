9d1d335ba6039d07f18e8ed39c50cbe27c1ca9b7
CyrilJcb <jacobcyril042@gmail.com>
2024-12-09 13:09:30 +0100

    premier observer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { RouterOutlet } from '@angular/router';
+import { interval } from 'rxjs';
 
 
 @Component({
@@ -13,6 +14,10 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  
+export class AppComponent  implements OnInit{
+  ngOnInit(): void {
+    const interval$ = interval(1000);
+    interval$.subscribe(value=>console.log(value))
+    setTimeout(()=>interval$.subscribe(value=>console.log(value)), 3000);
+  }
 }
